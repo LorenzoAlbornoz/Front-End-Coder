2e12c8d4e97a50031e99e6a3900ec034e6a99657
Lorenzo Albornoz <lorenzoa.28.la@gmail.com>
2024-02-18 14:45:00 -0300

    Mejoras en login

--- src/components/Login&register/Login.jsx
+++ src/components/Login&register/Login.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { LOGIN_SCHEMA } from "../../helpers/validationsSchemas";
-import { Link } from "react-router-dom";
+
 import { useNavigate } from 'react-router-dom';
 import { axiosInstance } from "../../config/axiosInstance";
 import { jwtDecode } from "jwt-decode";
@@ -128,17 +128,6 @@ const CustomLoader = () => (
               <p className="register__error-message">{errors.password.message}</p>
             )}
           </div>
-          <div className="login__enlace">
-            <span>
-              <Link to="/repassword" className="login__link">
-                {" "}
-                ¿Olvidaste tu contraseña?
-              </Link>
-            </span>
-          </div>
-          <button type="submit" className="login__button">
-            Entrar
-          </button>
         </form>
       )}
     </div>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "../components/Login&register/Login";
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
@@ -10,19 +11,30 @@ const LoginPage = () => {
         <div className="loginPage">
           <div className="loginPage__form">
             <h2 className="loginPage__createAccount">Iniciar Sesión</h2>
-            <div className="loginPage__iconos">
-              <div className="loginPage__borderIcon">
-                <a href="http://localhost:8080/api/facebook" className="loginPage__facebook">
-                  <BsFacebook />
-                </a>
-              </div>
-              <div className="loginPage__borderIcon">
-                <a href="http://localhost:8080/api/google" className="loginPage__google">
-                  <FcGoogle />
-                </a>
-              </div>
-            </div>
             <Login />
+            <div className="loginPage__iconos">
+                <div className="loginPage__borderIconFacebook">
+                  <a href="http://localhost:8080/api/facebook" className="loginPage__facebook">
+                    <BsFacebook /> Ingresar con Facebook
+                  </a>
+                </div>
+                <div className="loginPage__borderIconGoogle">
+                  <a href="http://localhost:8080/api/google" className="loginPage__google">
+                    <FcGoogle /> Ingresar con Google
+                  </a>
+                </div>
+              </div>
+              <div className="login__enlace">
+            <span>
+              <Link to="/repassword" className="login__link">
+                {" "}
+                ¿Olvidaste tu contraseña?
+              </Link>
+            </span>
+          </div>
+          <button type="submit" className="login__button">
+            Ingresar
+          </button>
             <div className="loginPage__enlace">
               <span>
                 ¿Aún no tienes una cuenta? <a href="/register" className="loginPage__link">Regístrate</a>
